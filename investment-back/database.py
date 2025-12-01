e212960f67b95938b434b2ccad35a0e3c4517970
ConverseDDULULU <alstjsrb2568@naver.com>
2025-12-01 17:17:44 +0900

    RDS update

--- investment-back/database.py
+++ investment-back/database.py
@@ -2,10 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-RDS_USER = "admin"
-RDS_PASSWORD = "EasyFundDB123!"
-RDS_HOST = "easyfund-db.c10gmuokacda.us-east-1.rds.amazonaws.com"
-RDS_DB = "investment"
+RDS_USER = os.getenv("RDS_USER", "admin")
+RDS_PASSWORD = os.getenv("RDS_PASSWORD", "EasyFundDB123!")  # 실제 접속에 성공한 비밀번호
+RDS_HOST = os.getenv("RDS_HOST", "easyfund-db.c10gmuokacda.us-east-1.rds.amazonaws.com")
+RDS_DB = os.getenv("RDS_DB", "investment")
+
 
 DATABASE_URL = f"mysql+pymysql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:3306/{RDS_DB}"
 
