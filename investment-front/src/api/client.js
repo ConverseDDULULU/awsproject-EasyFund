4341d32e3b817cb8275c51e2402b2c4083bef545
ConverseDDULULU <alstjsrb2568@naver.com>
2025-12-03 17:22:07 +0900

    aws api

--- investment-front/src/api/client.js
+++ investment-front/src/api/client.js
@@ -1,25 +1,11 @@
-// import axios from "axios";
-
-// // Prefer env override; default to nginx proxy path so front-end always calls backend through /api.
-// // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
-// const API_BASE_URL = "http://54.163.92.239:5000"; 
-
-// const client = axios.create({
-//   baseURL: API_BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default client;
-
-
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://54.163.92.239:5000",
+  // baseURL: "http://54.163.92.239:5000",
+  baseURL: "https://89g0gd9hml.execute-api.us-east-1.amazonaws.com",
   headers: { "Content-Type": "application/json" }
 });
 
 export default client;
+
 
