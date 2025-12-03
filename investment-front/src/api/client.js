ebf27fe1f3a5452f4a359f71622544539bd71a5a
ConverseDDULULU <alstjsrb2568@naver.com>
2025-12-03 19:50:14 +0900

    aws api

--- investment-front/src/api/client.js
+++ investment-front/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: "http://54.163.92.239:5000",
+  
   baseURL: "https://89g0gd9hml.execute-api.us-east-1.amazonaws.com",
   headers: { "Content-Type": "application/json" }
 });
